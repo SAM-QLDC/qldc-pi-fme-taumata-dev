7fdbdfdf3b4981b6d30a0b88f9df11af11f31cc4
Hywel Lewis <Hywel.Lewis@qldc.govt.nz>
2024-09-11 09:23:19 +1200

    batch update

--- run_workbench.bat
+++ run_workbench.bat
@@ -2,6 +2,8 @@
 
 SET fme2="C:\Program Files\ArcGIS\Data Interoperability for ArcGIS Pro\fme.exe"
 
-%fme2% %~dp0schemes.fmw  --DestDataset_CSV2 "C:\Github\qldc-pi-fme-taumata-dev\20241009_LWDW_RFI"  --FME_LAUNCH_VIEWER_APP "YES"
+%fme2% %~dp0schemes.fmw --DestDataset_CSV2 "C:\Github\qldc-pi-fme-taumata-dev" --FME_LAUNCH_VIEWER_APP "YES"
+
+%fme2% %~dp0network.fmw --DestDataset_CSV2 "C:\Github\qldc-pi-fme-taumata-dev" --FME_LAUNCH_VIEWER_APP "YES"
 
 PAUSE
